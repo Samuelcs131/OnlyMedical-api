f0ce78702f8e3084a09ae877fb98441a76053bbb
Samuel Claudino <samuelcs131@gmail.com>
2022-03-08 01:04:15 -0300

    ajuste index

--- src/index.js
+++ src/index.js
@@ -1,50 +1,28 @@
-import { async } from '@babel/runtime/regenerator'
 import { typeDefs, resolvers } from './grapql/merging'
+import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 const PORT = process.env.PORT || 8080
-/*
 import { ApolloServer } from 'apollo-server-express'
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import http from 'http'
 import express from 'express'
+import http from 'http'
 
-async function startApolloServer(){
-const PORT = process.env.PORT || 8080
-
-const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs, resolvers, plugins: [ApolloServerPluginDrainHttpServer({ httpServer }) ]
-  });
-
-
-await server.start();
-await server.applyMiddleware({ app });
-
-httpServer.listen({ port: PORT, path:'/graphql' }, ()=>{
-    console.log(`🚀 Servidor pronto em ${`http://localhost:${PORT}`}`)
-})
-
-}
-
-startApolloServer() */
-
-const { ApolloServer } = require('apollo-server-express')
-const express = require('express')
 const app = express()
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
-const server = new ApolloServer({ typeDefs, resolvers})
+app.get('/', (req,res)=>{  res.send('Servidor funcionando!')  })
 
-async function start(){
+async function start(){ 
+
     await server.start()
-    await server.applyMiddleware({app})
-
+    server.applyMiddleware({ app });
+    
     app.listen({ port: PORT }, ()=>{
     console.log("SERVER RUNNING ON PORT 8080")
 })
 }
-
-app.get('/', (req,res)=>{
-        res.send('Funcionando!')
-})
 
 start()
